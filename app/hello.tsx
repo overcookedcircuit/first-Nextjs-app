a3f7bb4ca287919b908b085f590823335f015234
overcookedcircuit <manaspatel31122004@gmail.com>
2025-09-16 10:12:18 -0400

    Hello world in nextjs

---
+++ app/hello.tsx
@@ -0,0 +1,9 @@
+// app/hello.tsx
+export default function Hello() {
+  return (
+    <>
+    <h1>Hello, Next.js!</h1>
+    <p>Welcome to your first ever new Next.js app. You can start editing the page by modifying <code>app/page.tsx</code>.</p>
+    </>
+);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import  Hello from './hello';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <Hello />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
